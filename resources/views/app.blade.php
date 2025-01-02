cb90341adce6185ff4b621720d4b9666b43acac5
sadaf <sadaf@klinkode.net>
2025-01-02 11:38:16 +0600

    added app bridge

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,8 +2,13 @@
 <html lang="en">
 
     <head>
+        <meta name="shopify-api-key" content="9b3df099635d5e4052a8087bf76e507f" />
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
+
         @viteReactRefresh
         @vite('resources/js/index.jsx', 'resources/css/app.css')
+
+
     </head>
 
     <body>
